e95334d26b3294abc273f3d571d6bde4c36caf05
Nikita739 <97597709+Nikita739@users.noreply.github.com>
2024-01-24 21:15:28 +0200

    CLIENT: Add Authorization flow

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import errorMiddleware from "./middleware/errorMiddleware";
 
 require("dotenv").config();
 
-import express, { Express, Request, Response } from "express";
+import express, {Express, NextFunction, Request, Response} from "express";
 import sequelize from './db';
 import router from "./routes/index";
 import cookieParser from 'cookie-parser';
@@ -11,12 +11,23 @@ import cors from 'cors';
 const app = express();
 const PORT: number = Number(process.env.PORT) || 4000;
 
+// app.use(cors({
+//     origin: "http://localhost:3000"
+// }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL
-}));
+// app.use(cors({
+//     credentials: true,
+//     origin: process.env.CLIENT_URL
+// }));
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header('Content-Type', 'application/json');
+    res.header("Access-Control-Allow-Methods", "OPTIONS,POST,GET,PATCH");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+})
 app.use('/api/v1', router);
 app.use(errorMiddleware);
 
